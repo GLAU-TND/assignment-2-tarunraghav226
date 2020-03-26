2219e0b48dcb01a63e030b224e6af2ea3a67a930
tarunraghav226 <tarun.raghav226@gmail.com>
2020-03-26 20:00:31 +0530

    Added insertion method in MyBinarySearchTree class.

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,47 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+
+    private TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
+    public void insert(TreeNode newNode) {
+
+        if (getRoot() == null) {
+            setRoot(newNode);
+            return;
+        }
+
+        TreeNode parent;
+        TreeNode current = getRoot();
+        boolean isLeft = true;
+
+        while (current != null) {
+            parent = current;
+            if (newNode.getData() > current.getData()) {
+                current = current.getRightNode();
+                isLeft = false;
+            } else {
+                current = current.getLeftNode();
+                isLeft = true;
+            }
+
+            if (isLeft)
+                parent.setLeftNode(newNode);
+            else
+                parent.setRightNode(newNode);
+        }
+    }
 }
