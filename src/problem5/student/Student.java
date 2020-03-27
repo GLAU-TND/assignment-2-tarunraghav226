b7ce92e8a65e52b97a8b07aa5c8bb39a9818b04f
tarunraghav226 <tarun.raghav226@gmail.com>
2020-03-27 16:18:54 +0530

    Removed problem3 Student class and completed problem5 Student class.

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,30 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    private String name;
+    private int rollNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "---------------------------------------" + "\n" +
+                "Name -- " + getName() + "\n" +
+                "Roll Number -- " + getRollNumber() + "\n" +
+                "---------------------------------------";
+    }
 }
