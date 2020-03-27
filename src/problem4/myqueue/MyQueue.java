f3a1a16593e71cab6ae4651997dd9d14da760698
tarunraghav226 <tarun.raghav226@gmail.com>
2020-03-27 17:55:01 +0530

    Added enQueue method.

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,5 +7,35 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+import problem4.node.Node;
+
 public class MyQueue {
+
+    private Node front;
+    private Node rear;
+
+    public Node getFront() {
+        return front;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
+
+    public Node getRear() {
+        return rear;
+    }
+
+    public void setRear(Node rear) {
+        this.rear = rear;
+    }
+
+    public void enQueue(Node newNode) {
+        if (front == null && rear == null) {
+            setFront(newNode);
+            setRear(newNode);
+        } else {
+            getRear().setNext(newNode);
+        }
+    }
 }
